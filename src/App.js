d2a88b985d96889c8a7a2ec4b1baa34464991a04
vkaash-sharma <vikasayushsharma@gmail.com>
2022-07-17 00:12:47 -0800

    :zap: favourites part added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import Banner from './Components/Banner';
 import List from './Components/List'
+import Favourites from './Components/Favourites';
 function App() {
   return (
    <>
     <Navbar />
-    <Banner />
-    <List />
+    {/* <Banner /> */}
+     {/* <List /> */}
+     <Favourites />
    </>
   );
 }
